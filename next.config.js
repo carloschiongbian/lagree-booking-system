bc12febee7ee87150074f5caa9d8f991be194845
Carlos Julian H. Chiongbian <carlosjulian.chiongbian@gmail.com>
2026-01-31 12:42:26 +0800

    chore: modified next config for cache control

--- next.config.js
+++ next.config.js
@@ -1,14 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  // output: "export",
-  experimental: { turbo: false }, // <- explicitly disable turbopack
+  experimental: { turbo: false },
   webpack: (config) => {
     config.module.exprContextCritical = false;
     return config;
   },
   eslint: { ignoreDuringBuilds: true },
   images: { unoptimized: true },
+
+  // Add this to improve caching of static chunks
+  async headers() {
+    return [
+      {
+        source: "/_next/static/:path*", // matches all static chunks
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable", // 1 year, immutable
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
